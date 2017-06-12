e98ba56ba203adf92ab2f888c65a87bf3e2c0dff
YASMIN NYGATE <YASMIN.NYGATE@GMAIL.COM>
2017-06-12 22:33:31 +0300

    worked a bit on map, changed logic to have google map elements in main render but with class hidden

--- app/components/map/map.js
+++ app/components/map/map.js
@@ -1,12 +1,13 @@
 import './map.scss';
 import React from 'react';
+import uuid from 'uuid';
 
 export default class Map extends React.Component {
   constructor() {
     super();
     this.state = {};
 
-    this.viewNewMap = this.viewNewMap.bind(this);
+    // this.viewNewMap = this.viewNewMap.bind(this);
     this.viewNewAddress = this.viewNewAddress.bind(this);
     this.viewNewLatlng = this.viewNewLatlng.bind(this);
     this.viewEditMode = this.viewEditMode.bind(this);
@@ -16,77 +17,98 @@ export default class Map extends React.Component {
     this.handleChangeAddress = this.handleChangeAddress.bind(this);
     this.handleChangeCoor = this.handleChangeCoor.bind(this);
     this.handelSubmit = this.handelSubmit.bind(this);
+    this.getLatLng = this.getLatLng.bind(this);
+    this.createCategories = this.createCategories.bind(this);
   }
 
   componentDidMount() {
     this.initMap();
   }
 
-  componentDidUpdate(){
-      this.initMap();
+  componentDidUpdate() {
+    this.initMap();
   }
 
-  viewNewMap() {
-    let marker;
-    let map = new google.maps.Map(this.mapElm, {
-      zoom: 13,
-      center: {lat: 32.07762926680949, lng: 34.79069709777832}
-    });
+  // viewNewMap() {
+  //   let marker;
+  //   let map = new google.maps.Map(this.mapElm, {
+  //     zoom: 13,
+  //     center: {lat: 32.07762926680949, lng: 34.79069709777832}
+  //   });
+  //
+  //   google.maps.event.addListener(map, 'click', function (event) {
+  //     const latlangNew = {lat: event.latLng.lat(), lng: event.latLng.lng()};
+  //     let locationObject: {
+  //       id: 0,
+  //       name: 'new',
+  //       address: '',
+  //       latLng: latlangNew,
+  //       relatedCat: [],
+  //       isEditMode: false
+  //     };
+  //     func(locationObject);
+  //     marker = new google.maps.Marker({
+  //       position: event.latLng,
+  //       map: map,
+  //       draggable: true,
+  //       animation: google.maps.Animation.DROP
+  //     });
+  //
+  //   });
+  // }
 
-    google.maps.event.addListener(map, 'click', function (event) {
-      const latlangNew = {lat: event.latLng.lat(), lng: event.latLng.lng()};
-      let locationObject: {
-        id: 0,
-        name: 'new',
-        address: '',
-        latLng: latlangNew,
-        relatedCat: [],
-        isEditMode: false
-      };
-      func(locationObject);
-      marker = new google.maps.Marker({
-        position: event.latLng,
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.DROP
-      });
-
-    });
+  handleChangeName() {
+    console.log();
   }
 
-  handleChangeName(){
-console.log();
-  }
-
-  handleChangeAddress(){
-
-  }
-
-  handleChangeCoor(){
+  handleChangeAddress() {
 
   }
 
-  handelSubmit(){
-console.log(this.mapElm);
+  handleChangeCoor() {
+
+  }
+
+  handelSubmit() {
+    console.log(this.mapElm);
   }
 
   viewNewAddress() {
+    this.addContainerElem.classList.remove('hidden');
+    let map = new google.maps.Map(this.mapElm, {
+      zoom: 8,
+      center: {lat: -34.397, lng: 150.644}
+    });
+    let geocoder = new google.maps.Geocoder();
 
-    return (<div>
-      <div id="floating-panel">
-        <input id="address" type="textbox" value="tel aviv"/>
-          <input id="submit" type="button" value="Find"/>
-      </div>
-    </div>);
+    this.submitElem.addEventListener('click', function () {
+      this.geocodeAddress(geocoder, map);
+    });
+  }
+
+  geocodeAddress(geocoder, resultsMap) {
+    let address = this.addElem.value;
+    geocoder.geocode({'address': address}, function (results, status) {
+      if (status === 'OK') {
+        console.log(results);
+        resultsMap.setCenter(results[0].geometry.location);
+        let marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      }
+      else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
   }
 
   viewNewLatlng() {
-
+    this.latLngContainerElem.classList.remove('hidden');
   }
 
-  //use ref to get new value of text!
-
   viewEditMode(locObj, handelSubmit) {
+    this.formElem.classList.remove('hidden');
     let templatePartOne = `<form onsubmit="handelSubmit()">
           <label>
             Name:
@@ -138,7 +160,7 @@ console.log(this.mapElm);
 
     infowindow.open(map, marker);
 
-    console.log();
+
   }
 
   viewRegular(locObj) {
@@ -186,9 +208,9 @@ console.log(this.mapElm);
   initMap(type, func) {
     const locationId = this.props.locationObject.id;
     if (locationId === 'new') {
-      if (type === 'map') {
-        return this.viewNewMap();
-      }
+      // if (type === 'map') {
+      //   return this.viewNewMap();
+      // }
       if (type === 'address') {
         return this.viewNewAddress();
       }
@@ -207,8 +229,53 @@ console.log(this.mapElm);
     }
   }
 
+  getLatLng(){
+    const newObject = {
+      id: uuid(),
+      name: 'New',
+      address: '?',
+      latLng: {lat: this.latElem.value, lng: this.lngElem.value},
+      relatedCat: [],
+      isEditMode: true
+    };
+    this.latLngContainerElem.classList.add('hidden');
+    this.viewEditMode(newObject);
+  }
+
+  createCategories(){
+
+  }
+
   render() {
-    return <div id="map" className="map" ref={(elem) => this.mapElm = elem }/>;
+    return <div>
+      <div id="form hidden" ref={(elem) => this.formElem = elem}>
+        <table>
+          <tr><td>Name:</td> <td><input type='text' id='name'/> </td> </tr>
+          <tr><td>Address:</td> <td><input type='text' id='address'/> </td> </tr>
+          <tr><td>Type:</td> <td><select id='type'> +
+            { this.createCategories() }
+            <option value='restaurant'>restaurant</option>
+          </select> </td></tr>
+          <tr><td/><td><input type='button' value='Save' onClick={}/></td></tr>
+        </table>
+      </div>
+      <div className="lat-lng-section hidden" ref={(elem) => this.latLngContainerElem = elem}>
+        <label>
+          Latitude:
+          <input type="number" ref={(elem) => this.latElem = elem}/>
+        </label>
+        <label>
+          Longitude:
+          <input type="number" ref={(elem) => this.lngElem = elem}/>
+        </label>
+        <button type="submit" onClick={ this.getLatLng }>Find</button>
+      </div>
+      <div className="floating-panel hidden" ref={(elem) => this.addContainerElem = elem}>
+        <input className="address" type="textbox" ref={(elem) => this.addElem = elem}/>
+        <input className="submit" type="button" value="Find" ref={(elem) => this.submitElem = elem}/>
+      </div>
+      <div id="map" className="map" ref={(elem) => this.mapElm = elem }/>
+    </div>;
   }
 
 }
